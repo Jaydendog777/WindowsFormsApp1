e1b2a42860e9b9f948e3cf7fea56ae3cb875f7ee
jayde <jayde@JAYDENLAPTOP77>
2024-05-08 14:17:28 -0400

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.p1Points = new System.Windows.Forms.Label();
+            this.p2Points = new System.Windows.Forms.Label();
+            this.winLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 15;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // p1Points
+            // 
+            this.p1Points.BackColor = System.Drawing.Color.Transparent;
+            this.p1Points.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.p1Points.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.p1Points.Location = new System.Drawing.Point(12, 386);
+            this.p1Points.Name = "p1Points";
+            this.p1Points.Size = new System.Drawing.Size(61, 54);
+            this.p1Points.TabIndex = 0;
+            this.p1Points.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // p2Points
+            // 
+            this.p2Points.BackColor = System.Drawing.Color.Transparent;
+            this.p2Points.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.p2Points.ForeColor = System.Drawing.Color.LawnGreen;
+            this.p2Points.Location = new System.Drawing.Point(923, 386);
+            this.p2Points.Name = "p2Points";
+            this.p2Points.Size = new System.Drawing.Size(61, 54);
+            this.p2Points.TabIndex = 1;
+            this.p2Points.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // winLabel
+            // 
+            this.winLabel.BackColor = System.Drawing.Color.Transparent;
+            this.winLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.winLabel.ForeColor = System.Drawing.Color.White;
+            this.winLabel.Location = new System.Drawing.Point(365, 40);
+            this.winLabel.Name = "winLabel";
+            this.winLabel.Size = new System.Drawing.Size(280, 54);
+            this.winLabel.TabIndex = 2;
+            this.winLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(996, 449);
+            this.Controls.Add(this.winLabel);
+            this.Controls.Add(this.p2Points);
+            this.Controls.Add(this.p1Points);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label p1Points;
+        private System.Windows.Forms.Label p2Points;
+        private System.Windows.Forms.Label winLabel;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,380 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        Rectangle player1 = new Rectangle(140, 175, 30, 30);
+        Rectangle player2 = new Rectangle(550, 175, 30, 30);
+        Rectangle puck = new Rectangle(360, 195, 20, 20);
+
+        Rectangle p1Bottem = new Rectangle(148, 202, 14, 7);
+        Rectangle p1Right = new Rectangle(137, 180, 7, 20);
+        Rectangle p1Left = new Rectangle(166, 180, 7, 20);
+        Rectangle p1Top = new Rectangle(149, 173, 14, 7);
+
+        Rectangle p2Bottem = new Rectangle(557, 202, 14, 7);
+        Rectangle p2Right = new Rectangle(548, 180, 7, 20);
+        Rectangle p2Left = new Rectangle(576, 180, 7, 20);
+        Rectangle p2Top = new Rectangle(557, 173, 14, 7);
+
+
+        Rectangle center = new Rectangle(360, 0, 10, 500);
+        Rectangle goal1 = new Rectangle(0, 160, 13, 60);
+        Rectangle goal2 = new Rectangle(735, 160, 13, 60);
+
+        int playerSpeed = 5;
+        int puckTotalSpeed = 5;
+        int puckSpeed = 5;
+        int puckSpeedX = 3;
+        int puckSpeedY = 3;
+        int puckHit = 0;
+
+        int p1score = 0;
+        int p2score = 0;
+
+        bool wPressed = false;
+        bool sPressed = false;
+        bool upPressed = false;
+        bool downPressed = false;
+
+        bool leftPressed = false;
+        bool rightPressed = false;
+        bool dPressed = false;
+        bool aPressed = false;
+
+        SolidBrush blueBrush = new SolidBrush(Color.DodgerBlue);
+        SolidBrush greenBrush = new SolidBrush(Color.LawnGreen);
+        SolidBrush redBrush = new SolidBrush(Color.Red);
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+        SolidBrush grayBrush = new SolidBrush(Color.Gray);
+        Pen border = new Pen(Color.White, 3);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wPressed = false;
+                    break;
+                case Keys.S:
+                    sPressed = false;
+                    break;
+                case Keys.Up:
+                    upPressed = false;
+                    break;
+                case Keys.Down:
+                    downPressed = false;
+                    break;
+                case Keys.Left:
+                    leftPressed = false;
+                    break;
+                case Keys.Right:
+                    rightPressed = false;
+                    break;
+                case Keys.A:
+                    aPressed = false;
+                    break;
+                case Keys.D:
+                    dPressed = false;
+                    break;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wPressed = true;
+                    break;
+                case Keys.S:
+                    sPressed = true;
+                    break;
+                case Keys.Up:
+                    upPressed = true;
+                    break;
+                case Keys.Down:
+                    downPressed = true;
+                    break;
+                case Keys.Left:
+                    leftPressed = true;
+                    break;
+                case Keys.Right:
+                    rightPressed = true;
+                    break;
+                case Keys.A:
+                    aPressed = true;
+                    break;
+                case Keys.D:
+                    dPressed = true;
+                    break;
+            }
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            puckHit++;
+            if (puckHit == 50)
+            {
+                if (puckSpeed > 0)
+                {
+                    puckHit = 0;
+                    puckSpeed--;
+                }
+            }
+
+            if (puck.IntersectsWith(p1Left))
+            {
+                puckSpeedX = puckTotalSpeed;
+                puckStuff();
+            }
+            if (puck.IntersectsWith(p1Top))
+            {
+                puckSpeedY = -puckTotalSpeed;
+                puckStuff();
+            }
+            if (puck.IntersectsWith(p1Bottem))
+            {
+                puckSpeedY = puckTotalSpeed;
+                puckStuff();
+            }
+            if (puck.IntersectsWith(p1Right))
+            {
+                puckSpeedX = -puckTotalSpeed;
+                puckStuff();
+            }
+
+            if (puck.IntersectsWith(p2Left))
+            {
+                puckSpeedX = puckTotalSpeed;
+                puckStuff();
+            }
+            if (puck.IntersectsWith(p2Top))
+            {
+                puckSpeedY = -puckTotalSpeed;
+                puckStuff();
+            }
+            if (puck.IntersectsWith(p2Bottem))
+            {
+                puckSpeedY = puckTotalSpeed;
+                puckStuff();
+            }
+            if (puck.IntersectsWith(p2Right))
+            {
+                puckSpeedX = -puckTotalSpeed;
+                puckStuff();
+            }
+
+
+
+            if (puck.Y >= 320)
+            {
+                puckSpeedY = -puckSpeed;
+            }
+            if (puck.Y <= 0)
+            {
+                puckSpeedY = puckSpeed;
+            }
+
+            if (puck.X <= 0)
+            {
+                puckSpeedX = puckSpeed;
+            }
+            if (puck.X >= 735)
+            {
+                puckSpeedX = -puckSpeed;
+            }
+
+            if (puckSpeed == 0)
+            {
+                puckSpeedX = puckSpeedY = 0;
+            }
+
+            if (puck.IntersectsWith(goal1))
+            {
+                if (p2score < 2) 
+                {
+                    p2score++;
+                    p2Points.Text = $"{p2score}";
+                    p2Points.Refresh();
+                    Thread.Sleep(1000);
+                    puck.X = 360;
+                    puck.Y = 195;
+                }
+                else
+                {
+                    winLabel.Text = "Player 2 wins!";
+                    winLabel.Refresh();
+                    p2score = 0;
+                    p1score = 0;
+                    p1Points.Text = $"{p1score}";
+                    p2Points.Text = $"{p2score}";
+                    p1Points.Refresh();
+                    p2Points.Refresh();
+                    Thread.Sleep(2000);
+                    puck.X = 360;
+                    puck.Y = 195;
+                }
+            }
+            if (puck.IntersectsWith(goal2))
+            {
+                if (p1score < 2)
+                {
+                    p1score++;
+                    p1Points.Text = $"{p1score}";
+                    p1Points.Refresh();
+                    Thread.Sleep(1000);
+                    puck.X = 360;
+                    puck.Y = 195;
+                }
+                else
+                {
+                    winLabel.Text = "Player 1 wins!";
+                    winLabel.Refresh();
+                    p2score = 0;
+                    p1score = 0;
+                    p1Points.Text = $"{p1score}";
+                    p2Points.Text = $"{p2score}";
+                    p1Points.Refresh();
+                    p2Points.Refresh();
+                    Thread.Sleep(2000);
+                    puck.X = 360;
+                    puck.Y = 195;
+                }
+            }
+
+            puck.X += puckSpeedX;
+            puck.Y += puckSpeedY;
+
+            keyCheck();
+            Refresh();
+        }
+
+        public void puckStuff()
+        {
+            puckSpeed = puckTotalSpeed;
+            puckHit = 0;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(grayBrush, center);
+            e.Graphics.FillRectangle(blueBrush, player1);
+            e.Graphics.FillRectangle(greenBrush, player2);
+            e.Graphics.FillRectangle(whiteBrush, puck);
+            e.Graphics.FillRectangle(grayBrush, goal1);
+            e.Graphics.FillRectangle(grayBrush, goal2);
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+
+        }
+        public void keyCheck()
+        {
+            if (upPressed == true)
+            {
+                if (player2.Y > 0)
+                {
+                    player2.Y -= playerSpeed;
+                    p2Bottem.Y -= playerSpeed;
+                    p2Right.Y -= playerSpeed;
+                    p2Top.Y -= playerSpeed;
+                    p2Left.Y -= playerSpeed;
+                }
+            }
+            if (downPressed == true)
+            {
+                if (player2.Y < 330)
+                {
+                    player2.Y += playerSpeed;
+                    p2Bottem.Y += playerSpeed;
+                    p2Right.Y += playerSpeed;
+                    p2Top.Y += playerSpeed;
+                    p2Left.Y += playerSpeed;
+                }
+            }
+            if (leftPressed == true)
+            {
+                if (player2.X > 370)
+                {
+                    player2.X -= playerSpeed;
+                    p2Bottem.X -= playerSpeed;
+                    p2Right.X -= playerSpeed;
+                    p2Top.X -= playerSpeed;
+                    p2Left.X -= playerSpeed;
+                }
+            }
+            if (rightPressed == true)
+            {
+                if (player2.X < 710)
+                {
+                    player2.X += playerSpeed;
+                    p2Bottem.X += playerSpeed;
+                    p2Right.X += playerSpeed;
+                    p2Top.X += playerSpeed;
+                    p2Left.X += playerSpeed;
+                }
+            }
+
+            if (wPressed == true)
+            {
+                if (player1.Y > 0)
+                {
+                    player1.Y -= playerSpeed;
+                    p1Bottem.Y -= playerSpeed;
+                    p1Right.Y -= playerSpeed;
+                    p1Top.Y -= playerSpeed;
+                    p1Left.Y -= playerSpeed;
+                }
+            }
+            if (sPressed == true)
+            {
+                if (player1.Y < 330)
+                {
+                    player1.Y += playerSpeed;
+                    p1Bottem.Y += playerSpeed;
+                    p1Right.Y += playerSpeed;
+                    p1Top.Y += playerSpeed;
+                    p1Left.Y += playerSpeed;
+                }
+            }
+            if (aPressed == true)
+            {
+                if (player1.X > 10)
+                {
+                    player1.X -= playerSpeed;
+                    p1Bottem.X -= playerSpeed;
+                    p1Right.X -= playerSpeed;
+                    p1Top.X -= playerSpeed;
+                    p1Left.X -= playerSpeed;
+                }
+            }
+            if (dPressed == true)
+            {
+                if (player1.X < 330)
+                {
+                    player1.X += playerSpeed;
+                    p1Bottem.X += playerSpeed;
+                    p1Right.X += playerSpeed;
+                    p1Top.X += playerSpeed;
+                    p1Left.X += playerSpeed;
+                }
+            }
+        }
+    }
+}
